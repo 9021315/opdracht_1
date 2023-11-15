3d3f9f714f7537b1efcd0fce1c0cb281079b177c
9021315 <144031506+9021315@users.noreply.github.com>
2023-11-15 13:26:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function acceptCookies() {
+    document.getElementById('cookie-wall').style.opacity = 0;
+    document.getElementById('cookie-wall').style.visibility = 'hidden';
+    document.getElementById('page-content').style.display = 'block';
+}
